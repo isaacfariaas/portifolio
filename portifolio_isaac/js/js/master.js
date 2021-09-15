ccdc1eb1196d04081cb8cf3f0bd27fe8b0471271
isaacfariaas <farias.isaac321@gmail.com>
2021-09-14 23:01:08 -0300

    v2-portifolio

---
+++ portifolio_isaac/js/js/master.js
@@ -0,0 +1,28 @@
+animatedProgressBar();
+	windowHieght();
+	contactFormValidation();
+	previewPannel();
+
+	function animatedProgressBar () {
+		$(".progress").each(function() {
+			var skillValue = $(this).find(".skill-lavel").attr("data-skill-value");
+			$(this).find(".bar").animate({
+				width: skillValue
+			}, 1500, "easeInOutExpo");
+
+			$(this).find(".skill-lavel").text(skillValue);
+		});
+	}
+
+	function windowHieght(){
+		if ( $(window).height() <=768 ) {
+			$(".pt-table").addClass("desktop-768");
+		} else {
+			$(".pt-table").removeClass("desktop-768");
+		}
+	}
+	
+	
+
+	
+	  
